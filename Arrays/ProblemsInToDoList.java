2d1ca27af9d906f3075b02ad009eb2527c9ca6bc
aniket1260 <anket1260@gmail.com>
2023-07-26 18:38:39 +0530

    Commiting files from Arrays

---
+++ Arrays/ProblemsInToDoList.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ProblemsinToDoList {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int remove = 0;
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            int numbers[] = new int[num];
+            for (int j = 0; j < numbers.length; j++) {
+                numbers[j] = sc.nextInt();
+                if (numbers[j] >= 1000) {
+                    remove++;
+                    System.out.println(remove);
+                    continue;
+                }
+            }
+        }
+
+    }
+}
